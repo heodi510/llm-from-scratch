5490ec094435d03b823eba227ede34158aa81936
heodi510 <wongsiuhei510@gmail.com>
2025-06-29 15:38:58 +0100

    Update testing modules

--- test/test_tokenizers.py
+++ test/test_tokenizers.py
@@ -20,8 +20,34 @@ import tiktoken
 from ScratchedModules.dataset import load_vocabs
 from ScratchedModules.tokenizer import SimpleTokenizerV1, SimpleTokenizerV2
 
-def test_simpletokenizer(tokenizer, text2ids):
-    tokenizer  = tokenizer(text2ids)
+
+def test_tiktokenizer():
+    print( "Testing Titoken....")
+    tokenizer = tiktoken.get_encoding('gpt2')
+    text = (
+    "Hello, do you like tea <|endoftext|> In the sunlut terraces"
+    "of someunknownPlace."
+    )
+    ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    print('Encoding normal text:')
+    print(ids)
+    print('Decoding ids back to text:')
+    print(tokenizer.decode(ids))
+    
+    text="AKwirw ier"
+    print(f'Encoding special text:  {text}')
+
+    ids = tokenizer.encode(text)
+    print(ids)
+    print('Decoding ids back to text:')
+    print(tokenizer.decode(ids))
+    
+def test_simpletokenizer_v1(pytestconfig):
+    print( "Testing SimpleTokenizerV1....")
+    project_root = pytestconfig.rootpath
+    data_file    = project_root / "data" / "the-verdict.txt"
+    text2ids=load_vocabs(path=data_file)
+    tokenizer  = SimpleTokenizerV1(text2ids)
     text = """"It's the last he painted, you know,"
             Mrs. Gisburn said with pardonable pride."""
     ids = tokenizer.encode(text)
@@ -40,37 +66,30 @@ def test_simpletokenizer(tokenizer, text2ids):
     except KeyError:
         # Handle missing words by mapping to the <unk> token index
         print(f"Contain word not found in vocabulary: {text}")
+    print("===========================")
 
-def test_tiktokenizer(tokenizer):
-    text = (
-    "Hello, do you like tea <|endoftext|> In the sunlut terraces"
-    "of someunknownPlace."
-    )
-    ids = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-    print('Encoding normal text:')
+def test_simpletokenizer_v2(pytestconfig):
+    print( "Testing SimpleTokenizerV2....")
+    project_root = pytestconfig.rootpath
+    data_file    = project_root / "data" / "the-verdict.txt"
+    text2ids=load_vocabs(path=data_file)
+    tokenizer  = SimpleTokenizerV2(text2ids)
+    text = """"It's the last he painted, you know,"
+            Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(text)
+    print('Encoding text included in voacb(text2ids):')
     print(ids)
     print('Decoding ids back to text:')
     print(tokenizer.decode(ids))
     
-    text="AKwirw ier"
-    print(f'Encoding special text:  {text}')
-
-    ids = tokenizer.encode(text)
-    print(ids)
-    print('Decoding ids back to text:')
-    print(tokenizer.decode(ids))
-
-
-def run():
-    text2ids=load_vocabs()
-    print( "Testing SimpleTokenizerV1....")
-    test_simpletokenizer(SimpleTokenizerV1, text2ids)
+    text = " Hello, do you like the tea"
+    print(f'Encoding text not included in voacb(text2ids):  {text}')
+    try:
+        ids = tokenizer.encode(text)
+        print(ids)
+        print('Decoding ids back to text:')
+        print(tokenizer.decode(ids))
+    except KeyError:
+        # Handle missing words by mapping to the <unk> token index
+        print(f"Contain word not found in vocabulary: {text}")
     print("===========================")
-    print( "Testing SimpleTokenizerV2....")
-    test_simpletokenizer(SimpleTokenizerV2, text2ids)
-    print("===========================")
-    tokenizer = tiktoken.get_encoding('gpt2')
-    test_tiktokenizer(tokenizer)
-
-if __name__ == "__main__":
-    run()
